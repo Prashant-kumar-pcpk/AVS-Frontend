fc31c817b51d4840972bcbeacb9a97948c0fc4fb
Prashant-kumar-pcpk <prashantkumar.pcpk@gmail.com>
2026-09-19 22:25:41 +0530

    Update SEO metadata for avsdeveloper.com

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -21,7 +21,7 @@ export default function sitemap() {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/contact`,
+      url: `${baseUrl}/contacts`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
